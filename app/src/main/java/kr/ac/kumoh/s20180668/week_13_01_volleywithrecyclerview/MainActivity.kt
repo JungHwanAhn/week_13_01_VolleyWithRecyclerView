0038a1e389d96262c04e2f35cb0f07b1846517bc
JungHwanAhn <wjdghks1831@naver.com>
2022-11-28 16:37:22 +0900

    :rocket: complete

---
+++ app/src/main/java/kr/ac/kumoh/s20180668/week_13_01_volleywithrecyclerview/MainActivity.kt
@@ -0,0 +1,60 @@
+package kr.ac.kumoh.s20180668.week_13_01_volleywithrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kr.ac.kumoh.s20180668.week_13_01_volleywithrecyclerview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var model: SongViewModel
+    private val songAdapter = SongAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        model = ViewModelProvider(this)[SongViewModel::class.java]
+
+        binding.list.apply {
+            layoutManager = LinearLayoutManager(applicationContext)
+            setHasFixedSize(true)
+            itemAnimator = DefaultItemAnimator()
+            adapter = songAdapter
+        }
+
+        model.list.observe(this) {
+            songAdapter.notifyItemRangeInserted(0, model.list.value?.size ?: 0)
+        }
+
+        model.requestSong()
+    }
+
+    inner class SongAdapter: RecyclerView.Adapter<SongAdapter.ViewHolder>() {
+        inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+            val txTitle: TextView = itemView.findViewById(android.R.id.text1)
+            val txSinger: TextView = itemView.findViewById(android.R.id.text2)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = layoutInflater.inflate(android.R.layout.simple_list_item_2,
+            parent,
+            false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.txTitle.text = model.list.value?.get(position)?.title ?: null
+            holder.txSinger.text = model.list.value?.get(position)?.singer ?: null
+        }
+
+        override fun getItemCount() = model.list.value?.size ?: 0
+    }
+}
